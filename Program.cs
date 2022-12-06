fcabe387cf8364632365a6cebe4213f73953f2f1
Saqwix <iva2012.lol@gmail.com>
2022-12-06 19:42:38 +0300

    task ввод array

--- Program.cs
+++ Program.cs
@@ -19,3 +19,40 @@ for (int i = 0; i < matrix.Length; i++)
     PrintArray(GetResArray(GetArrayLimitLength(matrix[i]), length_array));
     WriteLine("]");
 }
+
+string[] GetArrayLimitLength(string[] arr)
+{
+    string[] result = new string[arr.Length];
+    int k = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+            {
+                result[k] = arr[i];
+                k++;
+            }
+    }
+    return result;
+}
+
+
+int GetLengthArray(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] != null) count++;
+    }
+    return count;
+}
+
+
+string[] GetResArray(string[] arr, int length)
+{
+    string[] result = new string[length];
+    for (int i = 0; i < length; i++) 
+    {
+        result[i] = arr[i];
+    }
+    return result;
+}
